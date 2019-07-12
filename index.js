d55d597f0de0b4c2a91ade1828996b13edf98f55
Alejandro Saiz <alejandro.saizotero@gmail.com>
2019-07-12 15:43:47 +0200

    tenemos las vivisones y los contenidos,

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-console.log("Hello World Keepcoder");
+function divDeDivisor(number) {
+    var result = "";
+    if (number % 3 === 0) {
+        result += "Foo";
+    }
+    if (number % 5 === 0) {
+        result += "Bar";
+    }
+    if (number % 7 === 0) {
+        result += "quix";
+    }
+    return result
+
+
+}
+
+function conatDeNumeros(number) {
+    var result = "";
+    var j;
+    number = number.toString().split("");
+    for (j = 0; j < number.length; j += 1) {
+        if (number[j] === "3") {
+            result += "Foo";
+        } else if (number[j] === "5") {
+            result += "Bar";
+        } else if (number[j] === "7") {
+            result += "Quix";
+        }
+    }
+    return result;
+
+}
+
+
+
+
+console.log(divDeDivisor(5));
+console.log(conatDeNumeros(5));
+
+
